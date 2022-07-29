a9b05eb7714ab52f9238105480909f891237f30c
A233S <71011762+A233S@users.noreply.github.com>
2022-07-29 21:56:11 +0800

    Update addfile.sh

--- addfile.sh
+++ addfile.sh
@@ -5,6 +5,7 @@ sudo chmod a+x /bin/repo
 repo init -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0 --depth=1
 repo sync --force-sync
 rm -rf .repo
+rm -rf /home/runner/work/orangmake/orangmake//.repo
 echo "###ls"
 ls -a
 echo "###ls"
