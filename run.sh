1b16f7e4ce76860cddb3c40bc45ec4e8f9e4ee78
A233S <71011762+A233S@users.noreply.github.com>
2022-07-29 15:36:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 git config --global user.email "qq2136602722@gmail.com"
 git config --global user.name "A233S"
 git config --global user.password $password
+git init
+git remote add orangmake https://github.com/A233S/orangmake.git
+echo okkkkkk > ok.txt
+git add .
+git commit -m "add file"
+git push --set-upstream orangmake master
