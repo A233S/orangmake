9b806bdb9147e578862d17278deaaa5c3bdf7656
A233S <71011762+A233S@users.noreply.github.com>
2022-07-29 15:55:46 +0800

    Create addfile.sh

---
+++ addfile.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/bin
+curl https://mirrors.tuna.tsinghua.edu.cn/git/git-repo > ~/bin/repo
+sudo cp ~/bin/repo /bin/repo
+sudo chmod a+x /bin/repo
+repo init -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0 --depth=1
+repo sync --force-sync
