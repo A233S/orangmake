503af1edbc6c9d9dcf6516d1bd7a9435e720fbc4
A233S <71011762+A233S@users.noreply.github.com>
2022-07-29 18:14:25 +0800

    Update addfile.sh

--- addfile.sh
+++ addfile.sh
@@ -5,4 +5,6 @@ sudo chmod a+x /bin/repo
 repo init -u https://gitlab.com/OrangeFox/Manifest.git -b fox_9.0 --depth=1
 repo sync --force-sync
 git config http.version HTTP/1.1
+git config --global http.postBuffer 524288000
 git config http.postBuffer 524288000
+git config --global http.version HTTP/1.1
